9f9b4924af708717c6d7a8ac02ba0175be3e9f9d
Dexter Fitch <fitch.dexter@gmail.com>
2021-01-28 00:08:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  const usersURL = "http://localhost:3000/users";
+  const userData = {
+    name: name,
+    email: email
+  };
+  const usersConfig = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch(usersURL, usersConfig)
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(obj) {
+      console.log(obj);
+    })
+    .catch(function(error) {
+      console.log(error.message);
+  });
+};
